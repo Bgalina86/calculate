3260ed643e044b70840f9c7757cc5943e1958e67
g.smirnova <g.smirnova@bftcom.com>
2025-01-12 19:26:46 +0300

    new AlphabetOfLetters.java

---
+++ src/main/java/com/example/constClass/AlphabetOfLetters.java
@@ -0,0 +1,5 @@
+package com.example.constClass;
+
+public class AlphabetOfLetters {
+    public static final String ALPHABET_OF_LETTERS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮёйцукенгшщзхъфывапролджэячсмитьбю";
+}
